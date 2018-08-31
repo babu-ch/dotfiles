f5d97e51c42585d37f99975920282815a01e6f26
nena3 <nena3@example.com>
2018-08-31 12:56:49 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-ln -s .bashrc ~/.bashrc
+pwd=`pwd`
+
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+
+    echo "make symbolic $f"
+    ln -snfv "$pwd"/"$f" "$HOME"/"$f"
+done
