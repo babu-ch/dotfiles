a9f5528523c4126a43682dc53d39e883335d4964
nena3 <42715882+nena3@users.noreply.github.com>
2019-08-02 10:33:32 +0900

    test update

--- install.sh
+++ install.sh
@@ -2,6 +2,26 @@
 
 pwd=`pwd`
 
+echo '### profile setting start'
+if [ -e ~/.bash_profile ]; then
+	echo '.bash_profile exist'
+
+	grep 'source ~/.bashrc' ~/.bash_profile >> /dev/null
+	if [ $? -eq 0 ]; then
+		echo '.bashrc setting exist'
+	else
+		echo '.bashrc setting not exist'
+		echo 'append "source ~/.bashrc"'
+		echo 'source ~/.bashrc' >> ~/.bash_profile
+	fi
+else
+	echo '.bash_profile not exist'
+	echo 'create .bash_profile'
+	echo 'source ~/.bashrc' >> ~/.bash_profile
+fi
+echo '### profile setting end'
+
+echo '### synbolic setting start'
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
@@ -10,3 +30,6 @@ do
     echo "make symbolic $f"
     ln -snfv "$pwd"/"$f" "$HOME"/"$f"
 done
+echo '### synbolic setting end'
+
+source ~/.bash_profile
